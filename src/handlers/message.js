5770c45e961f5113e0c696f21df9ad63380acbfb
8rs1 <arssani.info@gmail.com>
2026-01-04 23:29:04 +0330

    fix: user cancel command bug

--- src/handlers/message.js
+++ src/handlers/message.js
@@ -1,12 +1,17 @@
 import { sendMessage, forwardMessage } from '../api/telegram.js';
-import { ADMIN_ID } from '../constants.js';
-
+import { ADMIN_ID, BOT_COMMANDS } from '../constants.js';
 export const handleStartCommand = async (token, message) => {
 	const text = `Hi ${message.from.first_name}, welcome to messenger bot.\nYou can use this bot for contacting with admin.`;
 	await sendMessage(token, message.chat.id, text);
 };
-
+export const handleInvalidMessage = async (token, message) => {
+	await sendMessage(token, message.chat.id, 'Invalid message!\nPlease use commands.');
+};
 export const handleUserMessageToAdmin = async (token, message) => {
+	if (message.text.toLowerCase() === BOT_COMMANDS.CANCEL) {
+		await handleInvalidMessage(token, message);
+		return;
+	}
 	// Forward user message to admin
 	await forwardMessage(token, message.chat.id, ADMIN_ID, message.message_id);
 	// Create inline keyboard for answer
@@ -31,7 +36,4 @@ export const handleUserMessageToAdmin = async (token, message) => {
 			message_id: message.message_id,
 		},
 	});
-};
-export const handleInvalidMessage = async (token, message) => {
-	await sendMessage(token, message.chat.id, 'Invalid message!\nPlease use commands.');
 };
